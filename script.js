ac630e1c8eaaab03a336ead3f81ed1e8f142d603
shajigholam <shajigholam@github.com>
2023-08-08 22:55:47 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// JavaScript for the contact form
+function validateForm(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const propertyType = document.getElementById('propertyType').value;
+    const bedrooms = document.getElementById('bedrooms').value;
+    const bathrooms = document.getElementById('bathrooms').value;
+    const priceRange = document.getElementById('priceRange').value;
+
+    if (!firstName || !lastName || !phone || !email || !propertyType || !bedrooms || !bathrooms || !priceRange) {
+        alert('Please fill out all fields.');
+        return false;
+    }
+
+    const agentEmail = 'walter.white@gmail.com';
+
+    // Construct email body
+    const emailBody = `
+        New Contact Form Submission:
+
+        First Name: ${firstName}
+        Last Name: ${lastName}
+        Phone: ${phone}
+        Email: ${email}
+        Property Type: ${propertyType}
+        Bedrooms: ${bedrooms}
+        Bathrooms: ${bathrooms}
+        Price Range: ${priceRange}
+    `;
+
+    // Send email (Note: This is a simplified example)
+    const mailtoLink = `mailto:${agentEmail}?subject=Contact Form Submission&body=${encodeURIComponent(emailBody)}`;
+
+    // Display success prompt
+    if (window.confirm('Registered Successfully. Do you want to send the email now?')) {
+        window.location.href = mailtoLink;
+    }
+
+    return false; // Prevent form submission
+}
+
+document.getElementById('contactForm').addEventListener('submit', validateForm);
